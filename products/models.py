3742088271c3b9e8f6642b9b7d0098d4ce1206d5
AmyOShea <amyoshea.lsad@gmail.com>
2021-09-02 11:31:14 +0000

    FEAT: add 'category' to products app

--- products/models.py
+++ products/models.py
@@ -1,7 +1,19 @@
 from django.db import models
 
 
+class Category(models.Model):
+    name = models.CharField(max_length=254)
+    friendly_name = models.CharField(max_length=254, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+    def get_friendly_name(self):
+        return self.friendly_name
+
+
 class Product(models.Model):
+    category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
     sku = models.CharField(max_length=254, null=True, blank=True)
     artist = models.CharField(max_length=254, null=True, blank=True)
     name = models.CharField(max_length=254)
