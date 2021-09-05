18970133e4054175ac1cf2fd1053a3829fe78924
AmyOShea <amyoshea.lsad@gmail.com>
2021-09-05 14:07:13 +0000

    FEAT: add bag context and delivery logic

--- ARTstop/settings.py
+++ ARTstop/settings.py
@@ -66,6 +66,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'bag.contexts.bag_contents',
             ],
             'builtins': [
                 'crispy_forms.templatetags.crispy_forms_tags',
@@ -157,3 +158,6 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+FREE_DELIVERY_THRESHOLD = 75
+STANDARD_DELIVERY = 15
