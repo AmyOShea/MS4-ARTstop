685b1a4bfd2617fce7d887e03575820a4f218c5f
AmyOShea <amyoshea.lsad@gmail.com>
2021-09-06 14:21:00 +0000

    CHG: update bag context processor

--- bag/views.py
+++ bag/views.py
@@ -10,12 +10,10 @@ def add_to_bag(request, item_id):
     redirect_url = request.POST.get('redirect_url')
     bag = request.session.get('bag', {})
 
-
     if item_id in list(bag.keys()):
         bag[item_id] += quantity
     else:
         bag[item_id] = quantity
 
     request.session['bag'] = bag
-    print(request.session['bag'])
     return redirect(redirect_url)
