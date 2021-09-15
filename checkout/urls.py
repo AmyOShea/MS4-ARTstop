5c79e4f8000dfc08f918732d31ce6e5049cf2fa8
AmyOShea <amyoshea.lsad@gmail.com>
2021-09-15 12:07:07 +0000

    FEAT: add checkout-success views and urls

--- checkout/urls.py
+++ checkout/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path('', views.checkout_view, name='checkout'),
     path('signin_guest', views.signin_guest, name='signin_guest'),
+    path('checkout_success/<order_number>', views.checkout_success, name='checkout_success'),
 ]
