ea26e657f1bf237c612be17f8925016a2b56d520
AmyOShea <amyoshea.lsad@gmail.com>
2021-09-15 11:02:03 +0000

    FEAT: add basic checkout functionality

--- ARTstop/settings.py
+++ ARTstop/settings.py
@@ -162,5 +162,10 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+# Stripe
 FREE_DELIVERY_THRESHOLD = 75
 STANDARD_DELIVERY = 15
+STRIPE_CURRENCY = 'EUR'
+
+STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', '')
+STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
--- checkout/static/checkout/js/stripe_elements.js
+++ checkout/static/checkout/js/stripe_elements.js
@@ -4,9 +4,11 @@
 
 */
 
-var stripe_public_key = $('#id_stripe_public_key').text().slice(1, -1);
-var client_secret = $('#id_client_secret').text().slice(1, -1);
-var stripe = Stripe(stripe_public_key);
+// Handles card errors
+
+var stripePublicKey = $('#id_stripe_public_key').text().slice(1, -1);
+var clientSecret = $('#id_client_secret').text().slice(1, -1);
+var stripe = Stripe(stripePublicKey);
 var elements = stripe.elements();
 var style = {
     invalid: {
@@ -16,3 +18,49 @@ var style = {
 };
 var card = elements.create('card', {style: style});
 card.mount('#card-element');
+
+card.addEventListener('change', function (event) {
+    var errorDiv = document.getElementById('card-errors')
+    if (event.error) {
+        var html = `
+            <span role="alert">
+                <i class="fas fa-exclamation-circle"></i>
+            </span>
+            <span><strong>${event.error.message}</strong></span>
+            `
+        $(errorDiv).html(html);
+    } else {
+        errorDiv.textContent = '';
+    }
+});
+
+// Handles form submit
+var form = document.getElementById('payment-form');
+
+form.addEventListener('submit', function(e) {
+    e.preventDefault();
+    card.update({ 'disabled': true});
+    $('#submit-button').attr('disabled', true);
+    stripe.confirmCardPayment(clientSecret, {
+        payment_method: {
+            card: card,
+        }
+    }).then(function(result) {
+        if (result.error) {
+            var errorDiv = document.getElementById('card-errors');
+            var html = `
+                <span role="alert">
+                    <i class="fas fa-exclamation-circle"></i>
+                </span>
+                <span><strong>${result.error.message}</strong></span>
+                `;
+            $(errorDiv).html(html);
+            card.update({ 'disabled': false});
+            $('#submit-button').attr('disabled', false);
+        } else {
+            if (result.paymentIntent.status === 'succeeded') {
+                form.submit();
+            }
+        }
+    });
+});
