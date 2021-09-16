7bac285eb0ddaae509fbac67540e4f5010293732
AmyOShea <amyoshea.lsad@gmail.com>
2021-09-16 12:17:03 +0000

    FEAT: add loading screen on payments auth

--- checkout/static/checkout/js/stripe_elements.js
+++ checkout/static/checkout/js/stripe_elements.js
@@ -41,6 +41,8 @@ form.addEventListener('submit', function(e) {
     e.preventDefault();
     card.update({ 'disabled': true});
     $('#submit-button').attr('disabled', true);
+    $('#payment-form').fadeToggle(100);
+    $('#loading-overlay').fadeToggle(100);
     stripe.confirmCardPayment(clientSecret, {
         payment_method: {
             card: card,
@@ -55,6 +57,8 @@ form.addEventListener('submit', function(e) {
                 <span><strong>${result.error.message}</strong></span>
                 `;
             $(errorDiv).html(html);
+            $('#payment-form').fadeToggle(100);
+            $('#loading-overlay').fadeToggle(100);
             card.update({ 'disabled': false});
             $('#submit-button').attr('disabled', false);
         } else {
