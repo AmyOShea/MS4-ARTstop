9d888339c87c4fce660c418eee9c21106c62c2bc
AmyOShea <amyoshea.lsad@gmail.com>
2021-09-20 13:37:03 +0000

    CHG: update secret key & set debug

--- ARTstop/settings.py
+++ ARTstop/settings.py
@@ -10,10 +10,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-hri8-2)ghj%__yn@dga@$hu8ni1upz4-8hyt5rsc^k7rvpzim3'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['ms4-artstop.herokuapp.com', 'localhost']
 
