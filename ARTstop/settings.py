5bc1a3afc04d99794b5e44866341e6d97ef51076
AmyOShea <amyoshea.lsad@gmail.com>
2021-09-20 13:30:34 +0000

    FEAT: add deployment settings

--- ARTstop/settings.py
+++ ARTstop/settings.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 import os
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -14,7 +15,7 @@ SECRET_KEY = 'django-insecure-hri8-2)ghj%__yn@dga@$hu8ni1upz4-8hyt5rsc^k7rvpzim3
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['ms4-artstop.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -108,12 +109,17 @@ WSGI_APPLICATION = 'ARTstop.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 
 # Password validation
---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn ARTstop.wsgi:application
