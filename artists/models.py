29ea143ea8a475be53cc6d642944feedb3d93d8d
AmyOShea <amyoshea.lsad@gmail.com>
2021-09-22 13:13:46 +0000

    FEAT: add artists views and template

--- artists/models.py
+++ artists/models.py
@@ -4,6 +4,7 @@ from django.db import models
 class Artist(models.Model):
     name = models.CharField(max_length=254)
     artist_statement = models.TextField()
+    image_url = models.URLField(max_length=1024, null=True, blank=True)
     image = models.ImageField()
 
     def __str__(self):
--- artists/views.py
+++ artists/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from .models import Artist
 
-# Create your views here.
+def all_artists(request):
+
+    artists = Artist.objects.all()
+
+    context = {
+        'artists': artists
+    }
+
+    return render(request, 'artists/artists.html', context)
