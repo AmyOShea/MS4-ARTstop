ccacbb5c8b50cf9b5d31821852e3c6375440bc4d
AmyOShea <amyoshea.lsad@gmail.com>
2021-09-23 17:47:58 +0000

    CHG: update view imports & add nav links

--- artists/views.py
+++ artists/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect, reverse
+from django.contrib import messages
+
 from .models import Artist
 from products.models import Product
 from .forms import ArtistForm
@@ -28,11 +30,26 @@ def artist_detail(request, artist_id):
 
 
 def add_artist(request):
-    form = ArtistForm
-    template = 'artists/add_artist.html'
 
+    if not request.user.is_superuser:
+        messages.error(request, 'You are not authorized to do that.')
+        return redirect(reverse('home'))
+
+    if request.method == 'POST':
+        form = ArtistForm(request.POST, request.FILES)
+        if form.is_valid():
+            artist = form.save()
+            messages.success(request, 'Artist added')
+            return redirect(reverse('artist_detail', args=[artist.id]))
+        else:
+            messages.error(request, 'Failed to add Artist. \
+                Please endure form is valid')
+    else:
+        form = ArtistForm()
+
+    template = 'artists/add_artist.html'
     context = {
-        'form': form
+        'form': form,
     }
 
     return render(request, template, context)
