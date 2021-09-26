6e66e7e508ab5c4fb4d250d2b643b4cfdc6db909
AmyOShea <amyoshea.lsad@gmail.com>
2021-09-26 13:02:40 +0000

    FEAT: add delivery info to faq view

--- faqs/views.py
+++ faqs/views.py
@@ -1,5 +1,16 @@
 from django.shortcuts import render
+from django.conf import settings
 
 
 def faqs(request):
-    return render(request, 'faqs/faqs.html')
+
+    free_delivery = settings.FREE_DELIVERY_THRESHOLD
+    delivery_cost = settings.STANDARD_DELIVERY
+    template = 'faqs/faqs.html'
+
+    context = {
+        'free_delivery': free_delivery,
+        'delivery_cost': delivery_cost
+    }
+
+    return render(request, template, context)
