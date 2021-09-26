f225f63c1af76a28f18ced576d647047b443d3e7
AmyOShea <amyoshea.lsad@gmail.com>
2021-09-26 14:14:35 +0000

    FEAT: add artist links to model, update template

--- artists/models.py
+++ artists/models.py
@@ -6,6 +6,10 @@ class Artist(models.Model):
     artist_statement = models.TextField()
     image_url = models.URLField(max_length=1024, null=True, blank=True)
     image = models.ImageField()
+    ig_url = models.URLField(max_length=1024, null=True, blank=True)
+    fb_url = models.URLField(max_length=1024, null=True, blank=True)
+    linkedin_url = models.URLField(max_length=1024, null=True, blank=True)
+    website_url = models.URLField(max_length=1024, null=True, blank=True)
 
     def __str__(self):
         return self.name
