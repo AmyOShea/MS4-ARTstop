f4d140effc82e1f7e28a7d879e4f8497bb9527f9
AmyOShea <amyoshea.lsad@gmail.com>
2021-09-28 10:40:35 +0000

    FEAT: add class_detail url, view and template

--- classes/views.py
+++ classes/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Class
 
 
@@ -11,3 +11,16 @@ def all_classes(request):
     }
 
     return render(request, 'classes/classes.html', context)
+
+
+def class_detail(request, class_id):
+
+    # can't use 'class' as variable name as is reserved word
+    a_class = get_object_or_404(Class, pk=class_id)
+
+    context = {
+        # changing 'a_class' to 'class' as it can be use in HTML template
+        'class': a_class,
+    }
+
+    return render(request, 'classes/class_detail.html', context)
