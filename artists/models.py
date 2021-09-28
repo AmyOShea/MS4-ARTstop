ffe2bc91a7db9b3dd2b9d214b8723dca6cfa2606
AmyOShea <amyoshea.lsad@gmail.com>
2021-09-28 18:28:14 +0000

    CHG: update artist details social media links

--- artists/models.py
+++ artists/models.py
@@ -6,8 +6,10 @@ class Artist(models.Model):
     artist_statement = models.TextField()
     image_url = models.URLField(max_length=1024, null=True, blank=True)
     image = models.ImageField()
-    ig_url = models.URLField(max_length=1024, null=True, blank=True)
-    fb_url = models.URLField(max_length=1024, null=True, blank=True)
+    instagram_url = models.URLField(max_length=1024, null=True, blank=True)
+    facebook_url = models.URLField(max_length=1024, null=True, blank=True)
+    youtube_url = models.URLField(max_length=1024, null=True, blank=True)
+    twitter_url = models.URLField(max_length=1024, null=True, blank=True)
     linkedin_url = models.URLField(max_length=1024, null=True, blank=True)
     website_url = models.URLField(max_length=1024, null=True, blank=True)
 
