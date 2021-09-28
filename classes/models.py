7f26d9804e66a9ecf0130cc2b9952e843915c4f1
AmyOShea <amyoshea.lsad@gmail.com>
2021-09-28 13:44:09 +0000

    CHG: update classes model

--- classes/models.py
+++ classes/models.py
@@ -15,12 +15,17 @@ class Class(models.Model):
         verbose_name_plural = 'Classes'
 
     level = models.ForeignKey('Level', null=True,
-                                 blank=True, on_delete=models.SET_NULL)
+                              blank=True, on_delete=models.SET_NULL)
     name = models.CharField(max_length=254)
     description = models.TextField()
+    length = models.DecimalField(max_digits=6, decimal_places=2,
+                                 blank=True, null=True)
+    cover_image_url = models.URLField(max_length=1024, null=True, blank=True)
+    cover_image = models.ImageField(null=True, blank=True)
     video = models.FileField(null=True, blank=True,
                              validators=[FileExtensionValidator(
-                                 allowed_extensions=['MOV', 'avi', 'mp4', 'webm', 'mkv'])])
+                                 allowed_extensions=['MOV', 'avi', 'mp4',
+                                                     'webm', 'mkv'])])
     video_url = models.URLField(max_length=1024, null=True, blank=True)
 
     def __str__(self):
