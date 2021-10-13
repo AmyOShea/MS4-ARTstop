6c92e8aa08ecfbd512bf3e450bfd917dc96c014c
AmyOShea <amyoshea.lsad@gmail.com>
2021-10-13 14:06:33 +0000

    CHG: add STATIC_ROOT to settings

---
+++ staticfiles/js/artist-image.js
@@ -0,0 +1,9 @@
+const img = document.getElementById('artist-img')
+let width = img.clientWidth;
+let height = img.clientHeight;
+
+if (width < height) {
+    img.classList.add("portrait")
+} else {
+    img.classList.add("landscape")
+}
