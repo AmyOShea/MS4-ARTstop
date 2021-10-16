8d6c3f3221f320b910e02700d4b75f06e4080c9b
AmyOShea <amyoshea.lsad@gmail.com>
2021-10-16 17:11:04 +0000

    BUG: remove autofoucs from allauth forms

--- static/js/script.js
+++ static/js/script.js
@@ -4,6 +4,7 @@ let currentUrl = window.location.href;
 const nav = document.getElementById("main-nav");
 const viewBag = document.getElementById("toast-viewbag-btn");
 const contShopping = document.getElementById("toast-cont-shopping-btn");
+const formUsername = document.getElementById("id_username");
 
 if (currentUrl === 'https://8000-emerald-marlin-uaty91sq.ws-eu17.gitpod.io/' || currentUrl === 'https://ms4-artstop.herokuapp.com/' ) {
     nav.classList.add("home-main-nav")
@@ -18,3 +19,8 @@ if (currentUrl === 'https://8000-emerald-marlin-uaty91sq.ws-eu17.gitpod.io/bag/'
         viewBag.style.display = "inline-block";
     }
 };
+
+// Override the autofocus bug on allauth forms
+if (formUsername) {
+    formUsername.blur();
+}
