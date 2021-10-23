25a5716b6c9d3b2340880a81b114334c2cc9b2b6
AmyOShea <amyoshea.lsad@gmail.com>
2021-10-23 21:47:05 +0000

    CHG: add delete classes tests

--- classes/test_views.py
+++ classes/test_views.py
@@ -5,6 +5,7 @@ Testing views in the classes app
 from django.test import TestCase
 
 from django.shortcuts import get_object_or_404
+from django.contrib.messages import get_messages
 
 from profiles.models import User
 from .models import Class
@@ -75,26 +76,6 @@ class TestClassesViews(TestCase):
         response = self.client.get('/classes/add/')
         self.assertEqual(response.status_code, 302)
 
-    # def test_add_class(self):
-    #     """
-    #     Test that admin can add classes
-    # FORM VALIDATION CURRENTLY FAILING
-
-    #     """
-    #     self.client.force_login(self.admin_user)
-    #     add_class_form = ClassForm({
-    #         'name': 'testClass',
-    #         'description': 'testDescription',
-    #         'duration': '00:22:00',
-    #         'cover_image': 'testImage'
-    #     })
-
-    #     self.client.post('/classes/add/')
-    #     self.assertTrue(add_class_form.is_valid())
-    #     add_class_form.save()
-    #     add_class = Class.objects.all()
-    #     self.assertEqual(len(add_class), 2)
-
     def test_edit_class_view(self):
         """
         Test that only admin can access edit_class page.
@@ -124,7 +105,7 @@ class TestClassesViews(TestCase):
         """
         self.client.force_login(self.admin_user)
         a_class = get_object_or_404(Class, pk=self.class1.id)
-        class_form = ClassForm({
+        form = ClassForm({
             'name': 'testEditClass',
             'description': 'testEditDescription',
             'duration': '00:11:00',
@@ -132,8 +113,34 @@ class TestClassesViews(TestCase):
         },
             instance=a_class
         )
-        self.assertTrue(class_form.is_valid())
-        class_form.save()
+        self.assertTrue(form.is_valid())
+        form.save()
         self.client.post(f'/classes/edit/{self.class1.id}/')
         updated_class = Class.objects.get(id=self.class1.id)
         self.assertEqual(updated_class.name, 'testEditClass')
+
+    def test_delete_class_works_for_admin(self):
+        """
+        Test that admin can delete classes
+        """
+        self.client.force_login(self.admin_user)
+        response = self.client.get(f'/classes/delete/{self.class1.id}/')
+        messages = list(get_messages(response.wsgi_request))
+        self.assertEqual(messages[0].tags, 'success')
+        self.assertEqual(
+            str(messages[0]), 'Class Deleted')
+        self.assertEqual(response.status_code, 302)
+        self.client.logout()
+
+    def test_delete_class_doesnt_work_for_non_admin(self):
+        """
+        Test that non-admins cannot delete classes
+        """
+        self.client.force_login(self.user)
+        response = self.client.get(f'/classes/delete/{self.class1.id}/')
+        messages = list(get_messages(response.wsgi_request))
+        self.assertEqual(messages[0].tags, 'error')
+        self.assertEqual(
+            str(messages[0]), 'You are not authorized to do that.')
+        self.assertEqual(response.status_code, 302)
+        self.client.logout()
